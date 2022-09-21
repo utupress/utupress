fce53fd8dbe8b0381f6cfe971a32112c00e75abe
Dedan Irungu <dedanirungu@gmail.com>
2022-09-21 16:36:56 +0300

    utupress component setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import Utupress from "./utupress.vue";
+
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("v-utupress", Utupress);
+}
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+Utupress.install = install;
+
+export default Utupress;
